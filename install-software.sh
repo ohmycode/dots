e6cad500709bc0264fef66806141e42ae23989c4
ohmycode <felixkoehler87@yahoo.de>
2016-01-30 13:02:56 +0000

    Update install-software.sh

--- install-software.sh
+++ install-software.sh
@@ -1,1 +1,21 @@
+#!/usr/bin/env bash
 
+# TO-DO: search for brew packages or write install scripts
+
+# Ask for the administrator password upfront
+sudo -v
+
+# - Xcode
+
+# Xcode CLI tools
+xcode-select --install
+
+# -  Dropbox
+# Firefox
+# Google Chrome
+# iTerm
+# Keka
+# LittleSnitch
+# Sequel Pro
+# Sublime Text
+# Vlc
